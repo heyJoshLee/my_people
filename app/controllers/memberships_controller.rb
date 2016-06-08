482662da91ddda95acf54fbe9173767e6e2efe58
jleewebdev <lee45276@gmail.com>
2016-06-08 13:22:00 +0800

    Add button switching with joining a group

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,16 +1,15 @@
 class MembershipsController < ApplicationController
   def create
-    group = Group.find_by(slug: params[:group_id])
-    membership = Membership.new(group_id: group.id, user_id: current_user.id, role: "user")
+    @group = Group.find_by(slug: params[:group_id])
+    @membership = Membership.new(group_id: @group.id, user_id: current_user.id, role: "user")
     respond_to do |format| 
       format.js do
-        render :error unless membership.save
+        render :error unless @membership.save
       end
     end
   end
 
   def destroy
-
     membership = Membership.find(params[:id])
     @group = Group.find(membership.group_id)
     membership.destroy
