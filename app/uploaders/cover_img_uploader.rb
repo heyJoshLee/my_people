2ca1fe73f3ec3bef71999a68d800c43586f0e485
jleewebdev <lee45276@gmail.com>
2016-06-12 15:06:00 +0800

    Remove image processing

--- app/uploaders/cover_img_uploader.rb
+++ app/uploaders/cover_img_uploader.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class CoverImgUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_fill: [665, 375]
+  # process resize_to_fill: [665, 375]
 
   def extension_white_list
     %w(jpg jpeg png)
